496e309e073f70dbf8ff195a62a29fd88526e234
dimaIslyamov <islyaamov.dimaa@gmail.com>
2026-04-03 20:54:22 +0200

    Home work completed.

--- homeWork_lesson(1_2)/homeWork_lesson_1_2.py
+++ homeWork_lesson(1_2)/homeWork_lesson_1_2.py
@@ -1,4 +1,22 @@
-# firs text
-some = "text"
+# Написать первую программу: программа должна выводить в консоль несколько строчек
+print("Hello, World")
+print("It is my first python program")
+print("And it is the end of my program")
+print("Bye-bye")
 
-print(some)
+
+print("----------------------------------")
+
+
+# Написать программу на Python, которая запрашивает у пользователя данные и производит расчеты.
+user_name = input("Введите ваше имя: ")
+print(f"Приветствую, {user_name}!")
+
+travel_distance = int(input("Введите расстояние поездки (km): "))
+fuel_consumption = int(input("Введите расход топлива вашего автомобиля на 100км: "))
+gasoline_prices = int(input("Введите стоимость 1/литра бензина в вашем регионе: "))
+
+amount_of_gasoline = travel_distance * fuel_consumption / 100
+total_coast = gasoline_prices * amount_of_gasoline / 1
+
+print(f"Для поездки вам понадобится {int(amount_of_gasoline)} литров бензина, это будет стоить {int(total_coast)} рублей")
