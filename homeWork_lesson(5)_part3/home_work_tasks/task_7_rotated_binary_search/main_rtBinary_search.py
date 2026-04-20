4082c9734426de288f3b00b1f7824818465b480e
dimaIslyamov <islyaamov.dimaa@gmail.com>
2026-04-20 07:27:48 +0200

    fix

--- homeWork_lesson(5)_part3/home_work_tasks/task_7_rotated_binary_search/main_rtBinary_search.py
+++ homeWork_lesson(5)_part3/home_work_tasks/task_7_rotated_binary_search/main_rtBinary_search.py
@@ -3,10 +3,10 @@ from home_work_tasks.task_7_rotated_binary_search.rotated_binary_search import r
 
 lenght_of_list = int(input("Какой длины вы хотите список: "))
 number_list = []
-counter = 0
+counter = 1
 
 while len(number_list) < lenght_of_list:
-    num = int(input(f"Введите {counter+1} значение: "))
+    num = int(input(f"Введите {counter} значение: "))
     number_list.append(num)
     counter += 1
 
@@ -14,7 +14,7 @@ target = int(input(f"У вас есть список {number_list}, "
                    f"какое значение вы хотите найти: "))
 
 index = rotated_binary_search(arr=number_list, target=target)
-# 5, 6, 7, 1, 2, 3, 4
+
 if index is not None:
     print(f"Значение {target} найдено по индексу {index}")
 else:
