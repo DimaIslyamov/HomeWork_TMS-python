c7ca643671e6d34345e33281a13371d38c6b9144
dimaIslyamov <islyaamov.dimaa@gmail.com>
2026-08-07 19:50:36 +0200

    finished: stage 9

--- MiniBlog_Lab/posts/views.py
+++ MiniBlog_Lab/posts/views.py
@@ -1,5 +1,6 @@
 """Views for the posts application."""
-from django.urls import reverse_lazy
+
+from django.urls import reverse_lazy, reverse
 from django.views.generic import (
     ListView,
     DetailView,
@@ -14,14 +15,30 @@ class PostListView(ListView):
     """Display the list of posts."""
 
     model = Post
+    template_name = 'posts/post_list.html'
     context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            is_published=True
+        ).order_by('-created_at')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Все публикации'
+
+        return context
 
 
 class PostDetailView(DetailView):
     """Display one post."""
 
     model = Post
+    template_name = 'posts/post_detail.html'
     context_object_name = 'post'
+
+    def get_queryset(self):
+        return Post.objects.filter(is_published=True)
 
 
 class PostCreateView(CreateView):
@@ -29,6 +46,7 @@ class PostCreateView(CreateView):
 
     model = Post
     form_class = PostForm
+    template_name = 'posts/post_form.html'
     success_url = reverse_lazy('posts:post_list')
 
 
@@ -37,7 +55,12 @@ class PostUpdateView(UpdateView):
 
     model = Post
     form_class = PostForm
-    success_url = reverse_lazy('posts:post_list')
+
+    def get_success_url(self):
+        return reverse(
+            'posts:post_detail',
+            kwargs={'pk': self.object.pk},
+        )
 
 
 class PostDeleteView(DeleteView):
