f138c15e2d36ad8b93cafa6aa64899f98bb3bf8a
dimaIslyamov <islyaamov.dimaa@gmail.com>
2026-09-11 06:56:49 +0200

    completed: Stage 4

--- EventHub_API_DRF/api/serializers.py
+++ EventHub_API_DRF/api/serializers.py
@@ -1,26 +1,23 @@
 from rest_framework import serializers
 
+from .models import Category, Event
 
-class EchoSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
-    topic = serializers.CharField(max_length=100)
 
-    def validate_topic(self, value):
-        if value.lower() == "php":
-            raise serializers.ValidationError(
-                "This topic is not allowed."
-            )
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            "id",
+            "name",
+            "slug",
+            "is_active",
+        ]
+        read_only_fields = [
+            "id",
+        ]
 
-        return value
 
-    def validate(self, attrs):
-        name = attrs.get("name")
-        topic = attrs.get("topic")
-
-        if name.lower() == topic.lower():
-            raise serializers.ValidationError(
-                "Name and topic must be different."
-            )
-
-        return attrs
-    
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = "__all__"
